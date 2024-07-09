214ae0cd42da81f6692a2898422ce0b84d89eac4
krazio-01 <mdamman.krazio@gmai.com>
2024-07-09 11:59:45 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ mongoose
 // Middleware setup
 app.use(
     cors({
-        origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     })
 );
