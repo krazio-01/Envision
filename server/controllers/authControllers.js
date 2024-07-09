989abb0f1198008fcc0e4a31350a559d32bfac2a
krazio-01 <mdamman.krazio@gmai.com>
2024-07-09 18:29:24 +0530

    Initial commit

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -103,6 +103,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
+            sameSite: 'None',
             maxAge: 24 * 60 * 60 * 1000, // 1 day
         });
 
