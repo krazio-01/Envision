64127f2ee6125f6b4e4e6383c40ba84426348e21
krazio <md.krazio@gmail.com>
2026-06-09 23:41:00 +0530

    Optimize email transport and template I/O

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,49 +1,56 @@
 import nodemailer from 'nodemailer';
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const deliverSmtpEmail = async (userEmail, subject, text, html) => {
-    try {
-        const transporter = nodemailer.createTransport({
+let transporter = null;
+
+const getTransporter = () => {
+    if (!transporter) {
+        transporter = nodemailer.createTransport({
             service: process.env.SERVICE,
             port: process.env.EMAIL_PORT,
-            secure: process.env.SECURE,
+            secure: process.env.SECURE === 'true',
             auth: {
                 user: process.env.USER,
                 pass: process.env.PASSWORD,
             },
         });
-
-        const mail_configs = {
-            from: `"Envision" <${process.env.USER}>`,
-            to: userEmail,
-            subject: subject,
-            text: text,
-            html: html,
-        };
-
-        await transporter.sendMail(mail_configs);
-    } catch (error) {
-        console.error('Error sending email:', error);
-        throw error;
     }
+    return transporter;
+};
+
+const deliverSmtpEmail = async (userEmail, subject, text, html) => {
+    const mailer = getTransporter();
+
+    const mail_configs = {
+        from: `"Envision" <${process.env.USER}>`,
+        to: userEmail,
+        subject: subject,
+        text: text,
+        html: html,
+    };
+
+    await mailer.sendMail(mail_configs);
 };
 
 const sendEmail = async (to, subject, templateFilename, variables = {}) => {
     try {
         const templatePath = path.join(__dirname, '..', 'templates', templateFilename);
-        let htmlContent = fs.readFileSync(templatePath, 'utf8');
+        let htmlContent = await fs.readFile(templatePath, 'utf8');
 
-        variables['year'] = new Date().getFullYear().toString();
+        const templateData = {
+            ...variables,
+            year: new Date().getFullYear().toString()
+        };
 
-        for (const [key, value] of Object.entries(variables)) {
-            const regex = new RegExp(`{{${key}}}`, 'g');
-            htmlContent = htmlContent.replace(regex, value);
-        }
+        htmlContent = htmlContent.replace(/{{(.*?)}}/g, (match, variableName) => {
+            const trimmedKey = variableName.trim();
+            return trimmedKey in templateData ? templateData[trimmedKey] : match;
+        });
 
         await deliverSmtpEmail(to, subject, '', htmlContent);
     } catch (error) {
